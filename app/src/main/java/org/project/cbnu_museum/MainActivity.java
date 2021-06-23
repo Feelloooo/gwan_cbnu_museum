c0a9e4a9b6aa7e28025c81e47cf3ed6116eeff2a
Feelloooo <giung5311@hanmail.net>
2021-06-23 19:54:07 +0900

    reverse direction cardview

--- app/src/main/java/org/project/cbnu_museum/MainActivity.java
+++ app/src/main/java/org/project/cbnu_museum/MainActivity.java
@@ -21,10 +21,10 @@ public class MainActivity extends AppCompatActivity {
                 R.drawable.test3
         };
 
-//        v_fllipper = findViewById(R.id.image_slide);
-//        for(int image : images) {
-//            fllipperImages(image);
-//        }
+        v_fllipper = findViewById(R.id.image_slide);
+        for(int image : images) {
+            fllipperImages(image);
+        }
 
     }
     public void fllipperImages(int image) {
@@ -32,11 +32,11 @@ public class MainActivity extends AppCompatActivity {
         imageView.setBackgroundResource(image);
 
         v_fllipper.addView(imageView);      // 이미지 추가
-        v_fllipper.setFlipInterval(2000);       // 자동 이미지 슬라이드 딜레이시간(1000 당 1초)
-        v_fllipper.setAutoStart(true);          // 자동 시작 유무 설정
+        v_fllipper.setFlipInterval(2000);   // 자동 이미지 슬라이드 딜레이시간(1000 당 1초)
+        v_fllipper.setAutoStart(true);      // 자동 시작 유무 설정
 
         // animation
-        v_fllipper.setInAnimation(this,android.R.anim.slide_in_left);
-        v_fllipper.setOutAnimation(this,android.R.anim.slide_out_right);
+        v_fllipper.setInAnimation(this,R.anim.slide_in_right);
+        v_fllipper.setOutAnimation(this,R.anim.slide_out_left);
     }
 }
