a7468001ae31194afc34cc8f579859509397bc06
Feelloooo <giung5311@hanmail.net>
2021-06-24 16:48:45 +0900

    kakao map API

---
+++ app/src/main/java/org/project/cbnu_museum/Activity_route.java
@@ -0,0 +1,54 @@
+package org.project.cbnu_museum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import net.daum.mf.map.api.*;
+
+public class Activity_route extends AppCompatActivity implements MapView.POIItemEventListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route);
+
+        //mapview
+        MapView mapView = new MapView(this);
+
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
+        //marker
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName("충북대학교 박물관");
+        marker.setTag(0);
+        marker.setMapPoint(MapPoint.mapPointWithGeoCoord(36.62775104579331, 127.45535206324857));
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin);
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        mapView.addPOIItem(marker);
+
+        mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(36.62775104579331, 127.45535206324857), 2, true);
+        mapView.setPOIItemEventListener(this);
+
+    }
+
+    //implements MapView.POIItemEventListener Override
+    @Override
+    public void onPOIItemSelected(MapView mapView, MapPOIItem mapPOIItem) {
+    }
+    @Override
+    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem) {
+        String uri = "kakaomap://look?p=" + "8117063";
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+        startActivity(intent);
+    }
+    @Override
+    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem, MapPOIItem.CalloutBalloonButtonType calloutBalloonButtonType) {
+    }
+    @Override
+    public void onDraggablePOIItemMoved(MapView mapView, MapPOIItem mapPOIItem, MapPoint mapPoint) {
+    }
+}
--- app/src/main/java/org/project/cbnu_museum/MainActivity.java
+++ app/src/main/java/org/project/cbnu_museum/MainActivity.java
@@ -2,9 +2,20 @@ package org.project.cbnu_museum;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ViewFlipper;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,17 +26,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //hashkey발급
+        //getHashKey();
+
         int images[] = {
                 R.drawable.main_img1,
                 R.drawable.main_img2,
                 R.drawable.main_img3,
                 R.drawable.main_img4
         };
-
         v_fllipper = findViewById(R.id.image_slide);
         for(int image : images) {
             fllipperImages(image);
         }
+
+        Button button1 = findViewById(R.id.menu5);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Activity_route.class);
+                startActivity(intent);
+            }
+        });
 
     }
     public void fllipperImages(int image) {
@@ -40,4 +62,26 @@ public class MainActivity extends AppCompatActivity {
         v_fllipper.setInAnimation(this,R.anim.slide_in_right);
         v_fllipper.setOutAnimation(this,R.anim.slide_out_left);
     }
+
+//    hashkey발급
+//        private void getHashKey(){
+//            PackageInfo packageInfo = null;
+//            try {
+//                packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+//            } catch (PackageManager.NameNotFoundException e) {
+//                e.printStackTrace();
+//            }
+//            if (packageInfo == null)
+//                Log.e("KeyHash", "KeyHash:null");
+//
+//            for (Signature signature : packageInfo.signatures) {
+//                try {
+//                    MessageDigest md = MessageDigest.getInstance("SHA");
+//                    md.update(signature.toByteArray());
+//                    Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+//                } catch (NoSuchAlgorithmException e) {
+//                    Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+//                }
+//            }
+//        }
 }
