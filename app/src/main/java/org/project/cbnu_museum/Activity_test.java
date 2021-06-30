84be9db9c8762a49620788ef7ddf1d75199aa60f
Feelloooo <giung5311@hanmail.net>
2021-06-30 15:54:38 +0900

    Database & Image loading test

--- app/src/main/java/org/project/cbnu_museum/Activity_test.java
+++ app/src/main/java/org/project/cbnu_museum/Activity_test.java
@@ -1,29 +1,80 @@
 package org.project.cbnu_museum;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 public class Activity_test extends AppCompatActivity {
 
     public List<User> userList;
+    ImageView imageView;
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
+        imageView = (ImageView)findViewById(R.id.test_imageview);
 
-        initLoadDB();
+        initLoadDB();   //데이터베이스 로딩
+
+
+        // 안드로이드에서 네트워크와 관련된 작업을 할 때,
+        // 반드시 메인 Thread가 아닌 별도의 작업 Thread를 생성하여 작업해야 한다.
+        Thread mThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(userList.get(0).getAddress());
+
+                    // Web에서 이미지를 가져온 뒤
+                    // ImageView에 지정할 Bitmap을 만든다
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setDoInput(true); // 서버로 부터 응답 수신
+                    conn.connect();
+
+                    InputStream is = conn.getInputStream(); // InputStream 값 가져오기
+                    bitmap = BitmapFactory.decodeStream(is); // Bitmap으로 변환
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        mThread.start(); // Thread 실행
+
+        try {
+            // 메인 Thread는 별도의 작업 Thread가 작업을 완료할 때까지 대기해야한다
+            // join()를 호출하여 별도의 작업 Thread가 종료될 때까지 메인 Thread가 기다리게 한다
+            mThread.join();
+
+            // 작업 Thread에서 이미지를 불러오는 작업을 완료한 뒤
+            // UI 작업을 할 수 있는 메인 Thread에서 ImageView에 이미지를 지정한다
+            imageView.setImageBitmap(bitmap);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         TextView text1;
         text1 = (TextView) findViewById(R.id.test_textview);
         text1.setText(userList.get(0).getName());
-
-
     }
 
     private void initLoadDB() {
