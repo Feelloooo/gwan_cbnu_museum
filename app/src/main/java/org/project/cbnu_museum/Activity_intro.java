c2af243647f8cb48212a1b3c7a95615f3660ad83
Feelloooo <giung5311@hanmail.net>
2021-07-06 17:14:05 +0900

    Activity_intro_4, Activity_intro_5

--- app/src/main/java/org/project/cbnu_museum/Activity_intro.java
+++ app/src/main/java/org/project/cbnu_museum/Activity_intro.java
@@ -26,6 +26,22 @@ public class Activity_intro extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        Button button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Activity_intro_3.class);
+                startActivity(intent);
+            }
+        });
+        Button button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Activity_intro_4.class);
+                startActivity(intent);
+            }
+        });
         Button button5 = findViewById(R.id.button5);
         button5.setOnClickListener(new View.OnClickListener() {
             @Override
